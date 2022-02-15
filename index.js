9c1f525a5a3fde6ac00d45246eaba7d5caed88e5
Drew Grey <dtsgrey@gmail.com>
2022-02-14 20:35:51 -0600

    rendered hero list

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-fetch("https://superhero-search.p.rapidapi.com/api/heroes")
-.then(response => {
-	console.log(response);
+document.addEventListener("DOMContentLoaded", function() {
+
+function fetchHeros(){
+fetch("https://superhero-search.p.rapidapi.com/api/heroes",{ 
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "superhero-search.p.rapidapi.com",
+		"x-rapidapi-key": "3fe5a2eed0msh72c15333815d476p14b3c3jsn112bff9aa40a"
+	}
 })
-.catch(err => {
-	console.error(err);
+.then(response => response.json())
+.then(data => renderCharacters(data))
+}
+
+function renderCharacters(charArray){
+	console.log(charArray)
+	const heroList = document.getElementById('hero-list')
+	charArray.forEach(hero => {
+		
+		const heroLi = document.createElement('li')
+		heroLi.textContent = hero.name
+		
+		const heroImg = document.createElement('img')
+		heroImg.setAttribute("src",hero.images.xs)
+		heroImg.setAttribute("id", hero.name)
+
+		heroLi.append(heroImg)
+		
+		heroList.append(heroLi)
+		
+		console.log(heroLi)
+	})
+}
+
+fetchHeros()
 });
+
+
+
+
+
+
