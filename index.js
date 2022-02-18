3f5dd0ff76bf35b2362f0e286a008933e5ae512b
Drew Grey <dtsgrey@gmail.com>
2022-02-18 10:19:40 -0600

    handled js side of hero refresh button

--- index.js
+++ index.js
@@ -1,89 +1,82 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-function fetchHeros(){
-fetch("https://superhero-search.p.rapidapi.com/api/heroes",{ 
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "superhero-search.p.rapidapi.com",
-		"x-rapidapi-key": "3fe5a2eed0msh72c15333815d476p14b3c3jsn112bff9aa40a"
-	}
-})
-.then(response => response.json())
-.then(data => renderHeros(data))
-}
-
-function renderHeros(heroArray){
-	const heroList = document.getElementById('hero-list')
-	heroArray.forEach(hero => {
-		
-		const heroLi = document.createElement('li')
-		heroLi.textContent = hero.name
-		
-		const heroImg = document.createElement('img')
-		heroImg.setAttribute("src",hero.images.xs)
-		heroImg.setAttribute("id", hero.name)
-
-		heroLi.append(heroImg)
-		heroList.append(heroLi)
-
-		heroLi.addEventListener("click", function(){
-			loadHero(hero)
+	function fetchHeros(){
+		fetch("https://superhero-search.p.rapidapi.com/api/heroes",{ 
+			"method": "GET",
+			"headers": {
+				"x-rapidapi-host": "superhero-search.p.rapidapi.com",
+				"x-rapidapi-key": "3fe5a2eed0msh72c15333815d476p14b3c3jsn112bff9aa40a"
+			}
 		})
-		
-	})
-}
-
-
-
-function fetchVillains(){
-fetch("https://superhero-search.p.rapidapi.com/api/villains", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "superhero-search.p.rapidapi.com",
-		"x-rapidapi-key": "3fe5a2eed0msh72c15333815d476p14b3c3jsn112bff9aa40a"
+		.then(response => response.json())
+		.then(data => renderHeros(data))
 	}
-})
-.then(response => response.json())
-.then(data => renderVillains(data))
-}
 
-function renderVillains(villainArray){
-	console.log(villainArray)
-	const villainList = document.getElementById('villain-list')
-	villainArray.forEach(villain => {
-		
-		const villainLi = document.createElement('li')
-		villainLi.textContent = villain.name
-		
-		const villainImg = document.createElement('img')
-		villainImg.setAttribute("src",villain.images.xs)
-		villainImg.setAttribute("id", villain.name)
-
-		//villainLi.style.listStyleImage = villain.image.xs
-		
-
-		villainLi.append(villainImg)
-		villainList.append(villainLi)
-
-		villainLi.addEventListener("click", function(){
+	function renderHeros(heroArray){
+		const heroList = document.getElementById('hero-list')
+		heroArray.forEach(hero => {
 			
-			loadVillain(villain)
+			const heroLi = document.createElement('li')
+			heroLi.textContent = hero.name
+			
+			const heroImg = document.createElement('img')
+			heroImg.setAttribute("src",hero.images.xs)
+			heroImg.setAttribute("id", hero.name)
+
+			heroLi.append(heroImg)
+			heroList.append(heroLi)
+
+			heroLi.addEventListener("click", function(){
+				loadHero(hero)
+			})
+			
 		})
-		
-	})
-}
-
-function loadPage(){
-	fetchHeros()
-	fetchVillains()
-}
-loadPage()
-
-});
-
-//basically we want to write 2 functions that grab a random send it to the spaces we decide in the browswer:
+	}
 
 
+
+	function fetchVillains(){
+		fetch("https://superhero-search.p.rapidapi.com/api/villains", {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "superhero-search.p.rapidapi.com",
+			"x-rapidapi-key": "3fe5a2eed0msh72c15333815d476p14b3c3jsn112bff9aa40a"
+		}
+		})
+		.then(response => response.json())
+		.then(data => renderVillains(data))
+	}
+
+	function renderVillains(villainArray){
+		console.log(villainArray)
+		const villainList = document.getElementById('villain-list')
+		villainArray.forEach(villain => {
+			
+			const villainLi = document.createElement('li')
+			villainLi.textContent = villain.name
+			
+			const villainImg = document.createElement('img')
+			villainImg.setAttribute("src",villain.images.xs)
+			villainImg.setAttribute("id", villain.name)
+
+			//villainLi.style.listStyleImage = villain.image.xs
+			
+
+			villainLi.append(villainImg)
+			villainList.append(villainLi)
+
+			villainLi.addEventListener("click", function(){
+				
+				loadVillain(villain)
+			})
+			
+		})
+	}
+
+
+
+
+	//basically we want to write 2 functions that grab a random send it to the spaces we decide in the browswer:
 	 function loadHero(displayHero){
     //     const heroStats = document.createElement("div")
     //     heroStats.id = `stats ${"powerstats"}`
@@ -104,7 +97,8 @@ loadPage()
 		heroImgDisplay.setAttribute("src", displayHero.images.md)
 		heroImgDisplay.setAttribute("id", displayHero.id)
     }
-    function loadVillain(displayVillain){
+    
+	function loadVillain(displayVillain){
 		console.log(displayVillain)
 
 		//const villainDisplayId = document.getElementById("villain-display")
@@ -114,43 +108,21 @@ loadPage()
 		villainImgDisplay.setAttribute("src", displayVillain.images.md)
 
 		villainImgDisplay.setAttribute("id", displayVillain.id)
-			
-		let btn = document.createElement("button")
-			btn.addEventListener('click', handleLike)
-			btn.textContent = 'Like'
-
-			let heroDiv = document.createElement('div')
-			heroDiv.append(btn)
-		}
-    function handleLike(e)    {
-    let iSpan = e.target.parentElement.querySelector('.like-span')
-  let number = parseInt(iSpan.textContent)
-
-  let newNum =  number + 1
-  iSpan.textContent = newNum
-  
-}
-
-	/*const heroSearchId = document.getElementById('hero-search')
-
-	heroSearchId.addEventListener("submit",function(event){
-		event.preventDefault()
-		heroSearch()
-	})
-	function heroSearch(){
-		const heroSearchName = document.getElementById("hero-search").value 
-		console.log(heroSearchName)
-
-
-		fetch(`https://superhero-search.p.rapidapi.com/api/?hero=${heroSearchName}`, {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "superhero-search.p.rapidapi.com",
-			"x-rapidapi-key": "3fe5a2eed0msh72c15333815d476p14b3c3jsn112bff9aa40a"
-		}
-		})
-		.then(response => response.json())
-		.then(data => console.log(data))
-
 	}
-	*/
+
+	const heroRefresh = document.getElementById("refresh-hero")
+
+	heroRefresh.addEventListener("click", function() {
+		const heroList = document.getElementById("hero-list")
+		heroList.innerHTML = ""
+		fetchHeros();
+	})
+  
+  
+	function loadPage(){
+		fetchHeros()
+		fetchVillains()
+	}
+	loadPage()
+})
+
